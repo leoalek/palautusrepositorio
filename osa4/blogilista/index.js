f1e097cca9b6620d1e085f39b13b0b2f45046f59
Oskari Aleksejev <oskari.aleksejev@tuni.fi>
2024-07-03 21:05:06 +0300

    controls: blogs

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -1,31 +1,15 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const Blog = require('./models/blog')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
+
+const blogsRouter = require('./controllers/blogs')
+app.use('/api/blogs', blogsRouter)
 
 
 app.use(cors())
 app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
